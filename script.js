5da6e24e521b34fc535fc6c54973eb40238a41f5
robinwatts96 <robinwatts96@gmail.com>
2021-08-11 11:35:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
 const deleteListButton = document.querySelector('[data-delete-list-button]');
+const listDisplayContainer = document.querySelector('[data-list-display-container]');
+const listTitleElement = document.querySelector('[data-list-title]');
+const listCountElement = document.querySelector('[data-list-count]');
+const tasksContainer = document.querySelector('[data-tasks]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedLIstId';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
@@ -47,10 +51,18 @@ function save() {
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
-
-
 function render() {
     clearElement(listsContainer);
+    renderLists();
+
+    if (selectedListId == null) {
+        listDisplayContainer.style.display = 'none';
+    } else {
+
+    }
+};
+
+function renderLists() {
     lists.forEach(list => {
         const listElement = document.createElement('li');
         listElement.dataset.listId = list.id;
@@ -61,7 +73,7 @@ function render() {
         }
         listsContainer.appendChild(listElement);
     }) 
-};
+}
 
 function clearElement(element) {
     while (element.firstChild) {
